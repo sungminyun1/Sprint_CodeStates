f430fb7716f529e90215ec031658c78c2878a20d
minjman2659 <minjman2659@gmail.com>
2021-06-27 15:58:18 +0900

    im-sprint-react-twittler-spa moved

---
+++ im-sprint-react-twittler-spa/src/Pages/Tweets.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { dummyTweets } from '../static/dummyData';
+import './Tweets.css';
+// ! 위 코드는 수정하지 않습니다.
+
+// TODO - import문을 이용하여 Tweet, Footer 컴포넌트를 불러오세요.
+
+const Tweets = () => {
+  return (
+    <React.Fragment>
+      <div className="tweetForm__container">
+        <div className="tweetForm__wrapper">
+          <div className="tweetForm__input">
+            <div className="tweetForm__inputWrapper">
+              <div className="tweetForm__count" role="status">
+                <span className="tweetForm__count__text">
+                  {'total: ' + dummyTweets.length}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul className="tweets">
+        TODO : 트윗 메세지가 있어야 합니다.
+      </ul>
+       TODO : Footer 컴포넌트를 작성합니다.
+    </React.Fragment>
+  );
+};
+
+export default Tweets;
