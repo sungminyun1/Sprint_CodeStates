bc528e2385f9871eda59cf7c4e1ee241e6720683
minjman2659 <minjman2659@gmail.com>
2021-07-01 19:03:18 +0900

    im-sprint-statesairline-server Solved

--- im-sprint-statesairline-server/statesairline/controller/bookController.js
+++ im-sprint-statesairline-server/statesairline/controller/bookController.js
@@ -7,6 +7,8 @@ module.exports = {
   // 전체 데이터 혹은 요청 된 flight_uuid, phone 값과 동일한 예약 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO:
+    console.log(req.query);
+
     const flight_uuid = req.query.flight_uuid;
     const phone = req.query.phone;
 
@@ -25,6 +27,8 @@ module.exports = {
   // 요청 된 예약 데이터를 저장합니다.
   create: async (req, res) => {
     //TODO: 
+    console.log(req.body);
+
     const {flight_uuid, name, phone} = req.body;
 
     const post = {
@@ -42,12 +46,15 @@ module.exports = {
   // 요청 된 phone 값과 동일한 예약 데이터를 삭제합니다.
   deleteById: async (req, res) => {
     //TODO: 
+    console.log(req.query);
+
     const phone = req.query.phone;
 
     if(phone) {
       booking = booking.filter((el) => el.phone !== phone);
+      return res.status(200).json(booking);
+    } else {
+      return res.status(404).send('Please Write Phone Number!');
     }
-  
-    return res.status(200).json(booking);
   }
 };
--- im-sprint-statesairline-server/statesairline/controller/flightController.js
+++ im-sprint-statesairline-server/statesairline/controller/flightController.js
@@ -5,6 +5,8 @@ module.exports = {
   // 요청 된 departure_times, arrival_times, destination, departure 값과 동일한 값을 가진 항공편 데이터를 조회합니다.
   findAll: async (req, res) => {
     //TODO: 
+    console.log(req.query);
+
     const departure_times = req.query.departure_times;
     const arrival_times = req.query.arrival_times;
     const destination = req.query.destination;
@@ -24,15 +26,27 @@ module.exports = {
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 조회합니다.
   findById: async (req, res) => {
     //TODO: 
+    console.log(req.params);
+    
     const id = req.params.id;
+    
+    const findData = flights.filter((el) => el.uuid === id);
 
-    return res.status(200).json(flights.filter((el) => el.uuid === id));
+    if(findData.length > 0) {
+      return res.status(200).json(findData);
+    } else {
+      return res.status(404).send('Not Found!');
+    }
+    
   },
 
   // [PUT] /flight/{:id} 요청을 수행합니다.
   // 요청 된 id 값과 동일한 uuid 값을 가진 항공편 데이터를 요청 된 Body 데이터로 수정합니다.
   update: async (req, res) => {
     //TODO: 
+    console.log(req.params);
+    console.log(req.body);
+
     const id = req.params.id;
     let data = req.body;
 
