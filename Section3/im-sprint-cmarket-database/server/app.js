7cd00d00835d72b07e93bf7c781446ea35dbc8a2
minjman2659 <minjman2659@gmail.com>
2021-07-26 18:06:03 +0900

    im-sprint-cmarket-database Solved

--- Section3/im-sprint-cmarket-database/server/app.js
+++ Section3/im-sprint-cmarket-database/server/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = require('./routes');
 const cors = require('cors');
 const morgan = require('morgan');
-const parser = require('body-parser');
 const controller = require('./controllers');
 
 const app = express();
@@ -12,7 +11,7 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
 app.use(cors());
-app.use(parser.json());
+app.use(express.json());
 app.use('/users', router);
 app.get('/items', controller.items.get);
 module.exports = app.listen(port, () => {
