0da65dd1006a654da115de9149b547b8698ab7ef
minjman2659 <minjman2659@gmail.com>
2021-07-27 19:54:40 +0900

    Recent Updated

--- Section3/im-sprint-cmarket-database/server/models/index.js
+++ Section3/im-sprint-cmarket-database/server/models/index.js
@@ -18,8 +18,9 @@ module.exports = {
     },
     post: (userId, orders, totalPrice, callback) => {
       // TODO: 해당 유저의 주문 요청을 데이터베이스에 생성하는 함수를 작성하세요
-      const sql1 = `insert into orders (user_id, total_price) values (${userId}, ${totalPrice})`
-      db.query(sql1, (err, result) => {
+      const sql1 = `insert into orders (user_id, total_price) values (?, ?)` // 템플릿 리터럴 방식으로도 작성 가능
+      const params = [userId, totalPrice];
+      db.query(sql1, params, (err, result) => {
         if(err) {
           callback(err, null);
         } else {
